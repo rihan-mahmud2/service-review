b930c8c7df02ad3db201883c4eecfe1337b3fb67
rihan-mahmud2 <rihanmahmud1415@gmail.com>
2022-11-10 01:14:02 +0600

    added deleted button

--- src/Pages/Myrivews/Mysinglerivews.js
+++ src/Pages/Myrivews/Mysinglerivews.js
@@ -2,10 +2,18 @@ import { TrashIcon } from "@heroicons/react/24/solid";
 import React from "react";
 
 const Mysinglerivews = ({ rivew }) => {
+  const handleDelete = () => {
+    fetch(`http://localhost:5000/rivews/${rivew._id}`, {
+      method: "DELETE",
+    });
+  };
   return (
     <tr>
       <th>
-        <TrashIcon className="text-sm w-10 text-red-400"></TrashIcon>
+        <TrashIcon
+          onClick={handleDelete}
+          className="text-sm w-10 text-red-400"
+        ></TrashIcon>
       </th>
       <td>{rivew?.name}</td>
       <td>{rivew.email}</td>
